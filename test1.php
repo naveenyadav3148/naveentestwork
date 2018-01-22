ff63de83e701ab170d9c407b10c2bc1468ef3705
Naveen Yadav <naveenyadav3148@gmail.com>
2018-01-22 12:50:46 +0530

    add hotfix line

--- test1.php
+++ test1.php
@@ -4,4 +4,7 @@ echo 'test1 and add some comment file in the base folder.';
 echo 'add new branch testing';
 echo 'add new line in another branch and testing for merge the branch';
 
+echo 'add new line working copy';
+echo 'add new line for hotfix';
+
 ?>
